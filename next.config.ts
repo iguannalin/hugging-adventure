5e3a1d90f8d7b6ecf1c2eee3008fefccf50b4855
seasons986 <annalin12345@gmail.com>
2025-05-16 14:40:12 -0400

    install hugging face

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // (Optional) Export as a static site
+  // See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
+  output: 'export', // Feel free to modify/remove this option
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // Override the default webpack configuration
+  webpack: (config) => {
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "sharp$": false,
+      "onnxruntime-node$": false,
+    }
+    return config;
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
